916c0964bd1feb73efe046a3c99ef2976ba4d454
Rima-khaleel <79520892+Rima-khaleel@users.noreply.github.com>
2021-02-24 09:20:21 -0800

    Testing

---
+++ JUnit/src/junit/MyTest.java
@@ -0,0 +1,11 @@
+package junit;
+
+public class MyTest {
+
+    public int add(String s1, String s2) {
+        int number1 = Integer.parseInt(s1);
+        int number2 = Integer.parseInt(s2);
+        return number1 + number2;
+    }
+
+}
